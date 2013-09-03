d6128d783117062f0c9b718fbe218e4d61277839
Brian Shoemaker <brianshoemaker@gmail.com>
2013-09-03 14:11:48 -0500

    Modified port configuration.

--- sample-config.js
+++ sample-config.js
@@ -1,4 +1,5 @@
 exports.config = {
+	port: 8083,
 	wundergroundKey: '',  // API key from Weather Underground API
 	wundergroundReferralUrl: 'http://www.wunderground.com/?apiref=',  // Referral URL to Weather Underground
 	geonamesUsername: '',  // Username with Geonames
