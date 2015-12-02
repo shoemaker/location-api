7b1fabb9ccd7c99bae5c223395f9202d30d90a26
Brian Shoemaker <brianshoemaker@gmail.com>
2015-12-02 08:38:24 -0600

    Updated package libraries. Cleanup.

--- controllers/forecast.js
+++ controllers/forecast.js
@@ -8,111 +8,111 @@ var c = require('../config').config;  // App configuration
 
 
 this.getForecast = function(locations, callback) {
-	
-	// Builds a function that handles the request to Forecast.io API. 
-	function addReq(loc) {
+    
+    // Builds a function that handles the request to Forecast.io API. 
+    function addReq(loc) {
 
-		// The actual function to return. 
-		return function(callback) { 
-			
-			// Define options for HTTP request to Forecast.io API.
-			var options = { 
-				host: 'api.forecast.io', 
-				path: '/forecast/<%=key%>/<%=lat%>,<%=lon%>?exclude=minutely,hourly,daily,flags'
-			};
-			options.path = encodeURI(_.template(options.path, {'key':c.forecastKey, 'lat':loc.latitude, 'lon':loc.longitude}));
+        // The actual function to return. 
+        return function(callback) { 
+            
+            // Define options for HTTP request to Forecast.io API.
+            var options = { 
+                host: 'api.forecast.io', 
+                path: '/forecast/<%=key%>/<%=lat%>,<%=lon%>?exclude=minutely,hourly,daily,flags'
+            };
+            options.path = encodeURI(_.template(options.path)({'key':c.forecastKey, 'lat':loc.latitude, 'lon':loc.longitude}));
 
-			// Check to see if we already have this result in cache
-			// Using node-cache: https://github.com/ptarjan/node-cache
-			var data = cache.get(options.path);  // Using the URL path as a key, check if cached response already exists. 
-			if (data) {
-				callback(null, data);  // We already have a cached response for this request, use it instead of hitting the Forecast.io API again. 
-			} else {
-				// No cached response exists, hit the Forecast.io API. 
-				var json = '';  // String to build up API response
-				https.get(options, function(res) { 
-					
-					// Handler for each chunk of data in the response from the Forecast.io API
-					res.on('data', function (chunk) { 
-						json += chunk;  // Append this chunk
-					});
-					
-					// Handler once the request to the Forecast.io API is complete. 
-					res.on('end', function() { 
-						var data = JSON.parse(json);  // Turn the string into an object. 
-						cache.put(options.path, data, c.cacheDuration);  // Put this response in cache in case we need it later. 
-						
-						callback(null, data);
-					});
-				}).on('error', function(err) {
-					console.log("Encountered error: " + err.message);
-					callback(err, loc);
-				});
-			}
-			
-		}  // END function for making API request. 
-	}  // END addReq(). 
-	
-	
-	var reqQueue = [];  // Array of Forecast.io API requests. 
-	for (var ii=0; ii<locations.length; ii++) {
-		reqQueue.push(addReq(locations[ii]));
-	}
-	
-	// Make series of requests to Forecast.io API.
-	// Order is important, we need to match order in array of locations. 
-	// Using async library: https://github.com/caolan/async
-	async.series(reqQueue, function(err, results) {
-		// Results received from the series of Forecast.io API requests.
-		
-		if (err) {
-			callback(err, results);
-		} else if (results.length != locations.length) {
-			callback('The number of Forecast.io results does not match the number of requests.', results);
-		}
+            // Check to see if we already have this result in cache
+            // Using node-cache: https://github.com/ptarjan/node-cache
+            var data = cache.get(options.path);  // Using the URL path as a key, check if cached response already exists. 
+            if (data) {
+                callback(null, data);  // We already have a cached response for this request, use it instead of hitting the Forecast.io API again. 
+            } else {
+                // No cached response exists, hit the Forecast.io API. 
+                var json = '';  // String to build up API response
+                https.get(options, function(res) { 
+                    
+                    // Handler for each chunk of data in the response from the Forecast.io API
+                    res.on('data', function (chunk) { 
+                        json += chunk;  // Append this chunk
+                    });
+                    
+                    // Handler once the request to the Forecast.io API is complete. 
+                    res.on('end', function() { 
+                        var data = JSON.parse(json);  // Turn the string into an object. 
+                        cache.put(options.path, data, c.cacheDuration);  // Put this response in cache in case we need it later. 
+                        
+                        callback(null, data);
+                    });
+                }).on('error', function(err) {
+                    console.log("Encountered error: " + err.message);
+                    callback(err, loc);
+                });
+            }
+            
+        }  // END function for making API request. 
+    }  // END addReq(). 
+    
+    
+    var reqQueue = [];  // Array of Forecast.io API requests. 
+    for (var ii=0; ii<locations.length; ii++) {
+        reqQueue.push(addReq(locations[ii]));
+    }
+    
+    // Make series of requests to Forecast.io API.
+    // Order is important, we need to match order in array of locations. 
+    // Using async library: https://github.com/caolan/async
+    async.series(reqQueue, function(err, results) {
+        // Results received from the series of Forecast.io API requests.
+        
+        if (err) {
+            callback(err, results);
+        } else if (results.length != locations.length) {
+            callback('The number of Forecast.io results does not match the number of requests.', results);
+        }
 
-		// Populate location object(s)
-		for (var ii=0; ii<results.length; ii++) {
-			var currWeather = results[ii];
-			locations[ii].weather.conditions = currWeather.currently.summary;
-			locations[ii].weather.icon = currWeather.currently.icon;
-			// locations[ii].weather.iconURL = currWeather.currently.icon_url;
-			locations[ii].weather.tempF = currWeather.currently.temperature;
-			locations[ii].weather.tempC = Math.round(((currWeather.currently.temperature - 32) * 5 / 9)*100)/100;
-			//locations[ii].weather.tempDescription = currWeather.temperature_string;
-			locations[ii].weather.humidity = currWeather.currently.humidity;
-			locations[ii].weather.windMPH = currWeather.currently.windSpeed;
-			locations[ii].weather.windDirection = currWeather.currently.windBearing + ' degrees';
-			// locations[ii].weather.windDescription = currWeather.wind_string;
-			locations[ii].weather.url = 'http://forecast.io/#/' + currWeather.latitude + ',' + currWeather.longitude;
-			locations[ii].weather.dataProvider = 'forecast.io';
-			locations[ii].weather.dataProviderUrl = 'https://developer.forecast.io/';
-			
-			// Using timezone offset data from Forecast.io
-			locations[ii].timeZone.offsetMS = convertOffsetToMS(currWeather.offset + '');
-			locations[ii].timeZone.offsetHours = (locations[ii].timeZone.offsetMS / (1000 * 60 * 60)) % 24;
-			// locations[ii].timeZone.standardName = currWeather.local_tz_long;
-			locations[ii].timeZone.shortName = currWeather.timezone;
-		}
-		
-		callback(null, locations);
-	});
+        // Populate location object(s)
+        for (var ii=0; ii<results.length; ii++) {
+            var currWeather = results[ii];
+            locations[ii].weather.conditions = currWeather.currently.summary;
+            locations[ii].weather.icon = currWeather.currently.icon;
+            // locations[ii].weather.iconURL = currWeather.currently.icon_url;
+            locations[ii].weather.tempF = currWeather.currently.temperature;
+            locations[ii].weather.tempC = Math.round(((currWeather.currently.temperature - 32) * 5 / 9)*100)/100;
+            //locations[ii].weather.tempDescription = currWeather.temperature_string;
+            locations[ii].weather.humidity = currWeather.currently.humidity;
+            locations[ii].weather.windMPH = currWeather.currently.windSpeed;
+            locations[ii].weather.windDirection = currWeather.currently.windBearing + ' degrees';
+            // locations[ii].weather.windDescription = currWeather.wind_string;
+            locations[ii].weather.url = 'http://forecast.io/#/' + currWeather.latitude + ',' + currWeather.longitude;
+            locations[ii].weather.dataProvider = 'forecast.io';
+            locations[ii].weather.dataProviderUrl = 'https://developer.forecast.io/';
+            
+            // Using timezone offset data from Forecast.io
+            locations[ii].timeZone.offsetMS = convertOffsetToMS(currWeather.offset + '');
+            locations[ii].timeZone.offsetHours = (locations[ii].timeZone.offsetMS / (1000 * 60 * 60)) % 24;
+            // locations[ii].timeZone.standardName = currWeather.local_tz_long;
+            locations[ii].timeZone.shortName = currWeather.timezone;
+        }
+        
+        callback(null, locations);
+    });
 }
 
 // Calculates the number of milliseconds for a provided offset (+HHMM). 
 function convertOffsetToMS(offset) {
-	var ms = 0;
-	
-	// +HHMM
-	var direction = offset.slice(0, 1);
-	var hours = offset.slice(1, 3);
-	var minutes = offset.slice(4, 5);
+    var ms = 0;
+    
+    // +HHMM
+    var direction = offset.slice(0, 1);
+    var hours = offset.slice(1, 3);
+    var minutes = offset.slice(4, 5);
 
-	ms = hours * 3600000;
-	ms += (minutes * 60000);
+    ms = hours * 3600000;
+    ms += (minutes * 60000);
 
-	if (direction == "-")
-		ms = ms * -1;
-		
-	return ms;
+    if (direction == "-")
+        ms = ms * -1;
+        
+    return ms;
 }
--- controllers/geonames.js
+++ controllers/geonames.js
@@ -11,96 +11,96 @@ var c = require('../config').config;  // App configuration
 
 
 this.getLocationDetails = function(locations, callback) {
-	
-	// Builds a function that handles the request to GeoNames API. 
-	function addReq(loc) {
+    
+    // Builds a function that handles the request to GeoNames API. 
+    function addReq(loc) {
 
-		// The actual function to return. 
-		return function(callback) { 
-			
-			// Define options for HTTP request to GeoNames API.
-			var options = { 
-				host: 'api.geonames.org', 
-				path: '/search?q=<%=location%>&type=json&maxRows=1&orderby=relevance&fuzzy=1&username=<%=username%>&isNameRequired=true'
-			};
-			options.path = encodeURI(_.template(options.path, {'location':loc, 'username':c.geonamesUsername}));
+        // The actual function to return. 
+        return function(callback) { 
+            
+            // Define options for HTTP request to GeoNames API.
+            var options = { 
+                host: 'api.geonames.org', 
+                path: '/search?q=<%=location%>&type=json&maxRows=1&orderby=relevance&fuzzy=1&username=<%=username%>&isNameRequired=true'
+            };
+            options.path = encodeURI(_.template(options.path)({'location':loc, 'username':c.geonamesUsername}));
 
-			// Check to see if we already have this result in cache
-			// Using node-cache: https://github.com/ptarjan/node-cache
-			var data = cache.get(options.path);  // Using the URL path as a key, check if cached response already exists. 
-			if (data) {
-				callback(null, data);  // We already have a cached response for this request, use it instead of hitting the GeoNames API again. 
-			} else {
-				// No cached response exists, hit the GeoNames API. 
-				var json = '';  // String to build up API response
-				http.get(options, function(res) { 
-					
-					// Handler for each chunk of data in the response from the GeoNames API
-					res.on('data', function (chunk) { 
-						json += chunk;  // Append this chunk
-					});
-					
-					// Handler once the request to the GeoNames API is complete. 
-					res.on('end', function() { 
-						var err;
-						try {
-							var data = JSON.parse(json);  // Turn the string into an object. 
-							// cache.put(options.path, data, c.cacheDuration);  // Put this response in cache in case we need it later. 
-							cache.put(options.path, data);  // This data doesn't change, don't set the cache to expire.
-						} catch(ex) {
-							console.log("Encountered error: " + ex.message);
-							err = ex;
-						} finally {
-							callback(err, data);
-						}							
-					});
-				}).on('error', function(err) {
-					console.log("Encountered error: " + err.message);
-					callback(err, loc);
-				});
-			}
-			
-		}  // END function for making API request. 
-	}  // END addReq(). 
-	
-	var reqQueue = [];  // Array of GeoNames API requests. 
-	for (var ii=0; ii<locations.length; ii++) {
-		reqQueue.push(addReq(locations[ii]));
-	}
-	
-	var locResults = []; // The aggregated responses from the GeoNames API. 
-	
-	// Make Parallel requests to GeoNames API. 
-	// Using async library: https://github.com/caolan/async
-	async.parallel(reqQueue, function(err, results) {
-		if (err) {
-			callback(err, null);
-		} else {		
-			// Results received from the series of GeoNames API requests.
-			// Populate location object(s)
-			if (results[0].totalResultsCount > 0) {
-				for (var ii=0; ii<results.length; ii++) {
-					var currLocation = results[ii].geonames[0];
-				
-					// Populate a new location object. 
-					var newLocation = models.location();
-						
-					newLocation.latitude = currLocation.lat;
-					newLocation.longitude = currLocation.lng;
-					newLocation.city = currLocation.toponymName;
-					newLocation.state = currLocation.adminName1;
-					newLocation.stateCode = currLocation.adminCode1;
-					newLocation.country = currLocation.countryName;
-					newLocation.countryCode = currLocation.countryCode;
-					newLocation.population = currLocation.population;
-					newLocation.dataProvider = 'GeoNames';
-					newLocation.dataProviderUrl = 'http://www.geonames.org/';
-				
-					locResults.push(newLocation);
-				}
-			}
-			callback(null, locResults);
-		}
-	});
+            // Check to see if we already have this result in cache
+            // Using node-cache: https://github.com/ptarjan/node-cache
+            var data = cache.get(options.path);  // Using the URL path as a key, check if cached response already exists. 
+            if (data) {
+                callback(null, data);  // We already have a cached response for this request, use it instead of hitting the GeoNames API again. 
+            } else {
+                // No cached response exists, hit the GeoNames API. 
+                var json = '';  // String to build up API response
+                http.get(options, function(res) { 
+                    
+                    // Handler for each chunk of data in the response from the GeoNames API
+                    res.on('data', function (chunk) { 
+                        json += chunk;  // Append this chunk
+                    });
+                    
+                    // Handler once the request to the GeoNames API is complete. 
+                    res.on('end', function() { 
+                        var err;
+                        try {
+                            var data = JSON.parse(json);  // Turn the string into an object. 
+                            // cache.put(options.path, data, c.cacheDuration);  // Put this response in cache in case we need it later. 
+                            cache.put(options.path, data);  // This data doesn't change, don't set the cache to expire.
+                        } catch(ex) {
+                            console.log("Encountered error: " + ex.message);
+                            err = ex;
+                        } finally {
+                            callback(err, data);
+                        }                           
+                    });
+                }).on('error', function(err) {
+                    console.log("Encountered error: " + err.message);
+                    callback(err, loc);
+                });
+            }
+            
+        }  // END function for making API request. 
+    }  // END addReq(). 
+    
+    var reqQueue = [];  // Array of GeoNames API requests. 
+    for (var ii=0; ii<locations.length; ii++) {
+        reqQueue.push(addReq(locations[ii]));
+    }
+    
+    var locResults = []; // The aggregated responses from the GeoNames API. 
+    
+    // Make Parallel requests to GeoNames API. 
+    // Using async library: https://github.com/caolan/async
+    async.parallel(reqQueue, function(err, results) {
+        if (err) {
+            callback(err, null);
+        } else {        
+            // Results received from the series of GeoNames API requests.
+            // Populate location object(s)
+            if (results[0].totalResultsCount > 0) {
+                for (var ii=0; ii<results.length; ii++) {
+                    var currLocation = results[ii].geonames[0];
+                
+                    // Populate a new location object. 
+                    var newLocation = models.location();
+                        
+                    newLocation.latitude = currLocation.lat;
+                    newLocation.longitude = currLocation.lng;
+                    newLocation.city = currLocation.toponymName;
+                    newLocation.state = currLocation.adminName1;
+                    newLocation.stateCode = currLocation.adminCode1;
+                    newLocation.country = currLocation.countryName;
+                    newLocation.countryCode = currLocation.countryCode;
+                    newLocation.population = currLocation.population;
+                    newLocation.dataProvider = 'GeoNames';
+                    newLocation.dataProviderUrl = 'http://www.geonames.org/';
+                
+                    locResults.push(newLocation);
+                }
+            }
+            callback(null, locResults);
+        }
+    });
 
 }
